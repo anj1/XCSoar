d04591e93e8a81aa19fc760e885088421519f8d7
Al Nejati <alireza.n.j@gmail.com>
2026-07-17 18:57:33 +1200

    Add Nickel cross-toolchain build support

--- build/thirdparty.py
+++ build/thirdparty.py
@@ -88,6 +88,19 @@ elif toolchain.is_android:
         libtiff,
         libgeotiff,
     ]
+elif host_triplet == 'arm-nickel-linux-gnueabihf':
+    thirdparty_libs = [
+        zlib,
+        libfmt,
+        libsodium,
+        freetype,
+        openssl,
+        libpng,
+        libjpeg,
+        cares,
+        curl,
+        lua,
+    ]
 elif '-kobo-linux-' in host_triplet:
     thirdparty_libs = [
         binutils,
